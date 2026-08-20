514eebe48febbbfee6da8160020403bbfcdac0a1
ShaliniShrivastava <sha.shri21@gmail.com>
2026-08-20 14:17:06 +0530

    new

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -168,11 +168,11 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
-
+    
     res.status(200).json({
       success: true,
       message: "Login successful",
